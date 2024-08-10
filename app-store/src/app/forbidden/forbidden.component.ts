4a81027ce50e1d769adaf223003b438878a459d7
Manoj1756 <53967953+Manoj1756@users.noreply.github.com>
2024-08-10 23:58:12 +0530

    Add files via upload

---
+++ app-store/src/app/forbidden/forbidden.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kp-forbidden',
+  templateUrl: './forbidden.component.html',
+  styleUrl: './forbidden.component.scss'
+})
+export class ForbiddenComponent {}
---
+++ app-store/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  oidc_url: './conf'
+};
